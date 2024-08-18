0a5501edd2d23bd8c965464a6e1753f10b8df2c4
Ansh gajbhiye <111952775+Anshmangithub@users.noreply.github.com>
2024-08-18 23:29:57 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaSquareXTwitter } from 'react-icons/fa6';
+import { FaInstagram } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+        <div className='flex flex-shrink-0 items-center'>
+            <img  className='max-w-28' src="ag.png" alt="" />
+        </div>
+        <div className='m-8 items-center justify-center flex gap-4 text-2xl'>
+            <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"><FaLinkedin/></a>
+           <a href="https://github.com/Anshmangithub"><FaGithub/></a> 
+           <a href="https://x.com/GajbhiyeAn52363"> <FaSquareXTwitter/></a>
+           <a href="https://www.instagram.com/anshhh.__g27_/"><FaInstagram/></a> 
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,73 @@
+
+
+export const HERO_CONTENT = `I am an enthusiastic full stack developer with expertise in the MERN stack, including React.js, Next.js, and JavaScript, as well as back-end technologies like Node.js, MongoDB, SQL, and Java. As a fresher, I am passionate about building responsive and dynamic web applications that deliver outstanding user experiences. With a strong foundation in both front-end and back-end development, I am eager to contribute innovative solutions that drive business growth and add value.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. As a fresher, I have developed a strong foundation in front-end technologies like React.js, Next.js, HTML, CSS, Tailwind CSS, and Redux, along with back-end expertise in Node.js, MongoDB, SQL, JavaScript, and Java. My journey in web development began with a deep curiosity for technology, which has grown into a commitment to continuous learning and adapting to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I love exploring new technologies and contributing to innovative projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "TalentForge Web Application",
+    image: "project-1.jpg",
+    description:
+      "A MERN stack platform connecting students with jobs and internships, where employers can create job postings. Features include authentication, session management, and CRUD operations.",
+    technologies: ["Next.js" , "Redux" , "Node.js" ,"Mongodb" , "Bootstrap"],
+  },
+  {
+    title: "BlogApp Web Application",
+    image: "project-2.jpg",
+    description:
+      "A web application that allows users to create, read, update, and delete blog posts. It features image uploads and user authentication for a seamless experience.",
+    technologies: ["React.js" ,"Redux", "Tailwind" ,"Appwrite"],
+  },
+  {
+    title: "VideoChat App",
+    image: "project-3.jpg",
+    description:
+      "A real-time web application that enables users to engage in video calls and chat seamlessly. With features like user authentication and instant messaging, it provides an interactive platform for communication. Users can easily connect with others, enhancing collaboration and social interaction.",
+    technologies: ["HTML", "CSS", "Node.js" ,"Express.js" , "Mongodb" ,"Bootstrap"],
+  },
+  {
+    title: "ZayaRum Website",
+    image: "project-4.jpg",
+    description:
+      "A responsive website designed for optimal user experience, featuring intuitive navigation and engaging layouts. The UI/UX design ensures seamless usability across devices, making it easy for visitors to access content and engage with the platform.",
+    technologies: ["HTML", "CSS", "Javascript" ,"jQuery" ],
+  },
+];
+
+export const CONTACT = {
+  address: "Waraseoni , Balaghat (M.P)",
+  phoneNo: "+91-7067850049 ",
+  email: "anshman5566@gmail.com",
+};
